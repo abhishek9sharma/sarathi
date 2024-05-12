b6c9994f0a9b960e1d112321248ca5204c3dd760
abhishek9sharma <abhishek9sharma@users.noreply.github.com>
2024-05-12 07:37:26 +0000

    Update version to 0.0.3 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, find_packages, setup
 
 setup(
     name="sarathi",
-    version="0.0.3a",
+    version="0.0.3",
     description="A CLI coding assistant",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
